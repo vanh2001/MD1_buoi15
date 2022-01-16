2de27791313697d0d4a2902854f85b0e06a79545
vanh2001 <anhviet1709@gmail.com>
2022-01-17 03:20:19 +0700

    Add file Convert

---
+++ convert.js
@@ -0,0 +1,12 @@
+//Convert feet to meter
+function footToMeter(foot) {
+    let meter;
+    return foot = 3.279 * meter;
+}
+
+//Convert meter to feet
+function meterToFoot(meter) {
+    let foot;
+    return meter = 0.305 * foot;
+}
+
