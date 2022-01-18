022423da287a583e57269c34fe40f06f42cb5436
vanh2001 <anhviet1709@gmail.com>
2022-01-18 17:36:15 +0700

    Update bai CRUD Product

--- manageProduct.js
+++ manageProduct.js
@@ -15,8 +15,8 @@ function showAllProduct() {
     for (let i = 0; i < products.length; i++) {
         let temp = "<tr>\n" +
             "            <td>"+ products[i] +"</td>\n" +
-            "            <td><button>Edit</button></td>\n" +
-            "            <td><button>Delete</button></td>\n" +
+            "            <td><button onclick='editProduct("+i+")'>Edit</button></td>\n" +
+            "            <td><button onclick='deleteProduct("+i+")'>Delete</button></td>\n" +
             "        </tr>";
         content += temp;
     }
@@ -26,15 +26,35 @@ function showAllProduct() {
 showAllProduct();
 
 //tạo mới sản phẩm:
-// 1. Tên hàm: createlProduct
+// 1. Tên hàm: createProduct
 // 2. Tham số: không có
 // 3. Thực thi:
 // Lấy tên sản phẩm từ ô input
 // Push phần tử mới vào mảng
 // Hiển thị danh sách lại
 // 4. Trả về: void
-function createlProduct() {
+function createProduct() {
     let name = document.getElementById('newProduct').value;
     products.push(name);
     showAllProduct();
+    document.getElementById('newProduct').value = "";
 }
+
+//xóa sản phẩm:
+// 1. Tên hàm: deleteProduct
+// 2. Tham số: không có
+// 3. Thực thi:
+// xóa phần tử
+// Hiển thị danh sách lại
+// 4. Trả về: void
+function deleteProduct(index) {
+    products.splice(index, 1);
+    showAllProduct();
+}
+
+function editProduct(index) {
+    let newName = prompt("Mời bạn nhập tên sản phẩm: ", products[index]);
+    products[index] = newName;
+    showAllProduct();
+}
+
